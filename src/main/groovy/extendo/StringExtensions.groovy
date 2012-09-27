7a8fdde8ce6357304f2a93eff9344afa4d810441
Bryan Chug <bryan.chug@aestrea.com>
2012-09-28 06:08:57 +0800

    Added String case extensions

--- src/main/groovy/extendo/StringExtensions.groovy
+++ src/main/groovy/extendo/StringExtensions.groovy
@@ -1,9 +1,28 @@
 package extendo
 
+import org.apache.commons.lang3.text.WordUtils
+
 class StringExtensions {
 
     static Date toDate( String self ){
         new Date( self )
     }
 
+    /* Case Extensions */
+    static Boolean isUpperCase( String self ){
+        self.toUpperCase() == self
+    }
+
+    static Boolean isLowerCase( String self ){
+        self.toLowerCase() == self
+    }
+
+    static Boolean isMixedCase( String self ){
+        !isLowerCase( self ) && !isUpperCase( self )
+    }
+
+    static String toTitleCase( String self ){
+        WordUtils.capitalizeFully( self )
+    }
+		
 }
--- src/test/groovy/extendo/StringExtensionsSpec.groovy
+++ src/test/groovy/extendo/StringExtensionsSpec.groovy
@@ -4,17 +4,69 @@ import spock.lang.Specification
 
 class StringExtensionsSpec extends Specification {
 
-	def 'Test date String as Date'(){
+    def 'Test date String as Date'() {
         expect:
-        dateString.toDate() == new Date( dateString )
+        dateString.toDate() == new Date(dateString)
 
         where:
         dateString << [
-            "Jan 1, 1990",
-            "Feb 20, 2012",
-            "November 21, 1988",
-            "October 2, 1989"
+                "Jan 1, 1990",
+                "Feb 20, 2012",
+                "November 21, 1988",
+                "October 2, 1989"
         ]
-	}
+    }
+
+    def "Test isUpperCase"() {
+        expect:
+        string.isUpperCase() == isUpperCase
+
+        where:
+        string        | isUpperCase
+        "HELLO WORLD" | true
+        "HELLO world" | false
+        "hello WORLD" | false
+        "hElLo WorLD" | false
+        "hello world" | false
+    }
+
+    def "Test isLowerCase"() {
+        expect:
+        string.isLowerCase() == isLowerCase
+
+        where:
+        string        | isLowerCase
+        "HELLO WORLD" | false
+        "HELLO world" | false
+        "hello WORLD" | false
+        "hElLo WorLD" | false
+        "hello world" | true
+    }
+
+    def "Test isMixedCase"() {
+        expect:
+        string.isMixedCase() == isMixedCase
+
+        where:
+        string        | isMixedCase
+        "HELLO WORLD" | false
+        "HELLO world" | true
+        "hello WORLD" | true
+        "hElLo WorLD" | true
+        "hello world" | false
+    }
+
+    def "Test toTitleCase"() {
+        expect:
+        string.toTitleCase() == titleCase
+
+        where:
+        string                                  | titleCase
+        "HELLO WORLD"                           | "Hello World"
+        "a new hope"                            | "A New Hope"
+        "the gods THEMSELVES"                   | "The Gods Themselves"
+        "The lion, the witch, and the wardrobe" | "The Lion, The Witch, And The Wardrobe"
+        "of MICE and MEN"                       | "Of Mice And Men"
+    }
 
 }
